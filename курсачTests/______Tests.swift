8c85750607e2edc14304e85c87f70ea136b961f3
Ust1111 <ustiabeseleva@gmail.com>
2025-04-30 13:24:15 +0300

    Initial Commit

---
+++ курсачTests/______Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ______Tests.swift
+//  курсачTests
+//
+//  Created by Устинья Беселева on 30.04.2025.
+//
+
+import Testing
+@testable import ______
+
+struct ______Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
